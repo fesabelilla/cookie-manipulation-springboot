10059846318d217f1704aeb50a83b783b8ab51ea
fesabelilla <fesabelilla@gmail.com>
2024-02-03 00:14:55 +0600

    Cookie Set

---
+++ cookie/src/main/java/com/example/cookie/CookieController.java
@@ -0,0 +1,20 @@
+package com.example.cookie;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Reff : https://websparrow.org/spring/spring-boot-cookies-example
+@RestController
+public class CookieController {
+
+    @GetMapping("/set")
+    public String setCookie(HttpServletResponse response) {
+        Cookie cookie = new Cookie("test", "1_2_3_4_5");
+        response.addCookie(cookie);
+        return "Spring Boot Cookies";
+    }
+
+}
